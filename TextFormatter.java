b7e932f11020c0a405f0a818697fb910bdb461cb
repl.it user <replituser@example.com>
2020-10-23 11:03:30 +0000

    committing unperfect version of Aufgabe2

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,3 +1,4 @@
+import StringTokenizer;
 class TextFormatter {
 
   private static final String text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy " +
@@ -7,6 +8,7 @@ class TextFormatter {
           "tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et " +
           "justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est " +
           "Lorem ipsum dolor sit amet.";
+    private int maxLineLength;      
 
   public static void main(String[] args) {
     TextFormatter formatter = new TextFormatter(30);
@@ -15,18 +17,23 @@ class TextFormatter {
 
   // Konstruktor
   public TextFormatter(int maxLineLength) {
-    int x = text.length()/ maxLineLength;
-    for (int i = 0; i<x; i++)
-    {
-      String a = text.substring(i*maxLineLength,(i+1)*maxLineLength);
-      System.out.println(a);
-    }
+    this.maxLineLength = maxLineLength;
   }
-
-   
 
   // Ausgabe
   public void print(String aText) {
+    int x = aText.length() / maxLineLength;
+    System.out.println("Amount of used chars: " + text.length());
+    System.out.println("Amount of lines: " + x + " divided by: " + maxLineLength);
+    for (int i = 0; i<x; i++)
+    {
+      //StringTokenizer toki = new StringTokenizer();
+      //while(toki.hasMoreTokens){
+        //if(toki.nextToken())
+      //}
+      aText = text.substring(i*maxLineLength,(i+1)*maxLineLength);
+      System.out.println(i + " " + aText);
+    }
     System.out.println(aText);
   }
 
